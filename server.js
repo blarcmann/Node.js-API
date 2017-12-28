78f1dfa67ccd4b5babded2f7dda74864194078ce
Blarcmann <dtl121ois@gmail.com>
2017-12-28 11:50:39 +0100

    Added Updating and Deleting

--- server.js
+++ server.js
@@ -19,7 +19,16 @@ app.get('/', function (req, res) {
 //Getting all todos
 
 app.get('/todos', function (req, res) {
-   res.json(todos); 
+    var queryParams = req.query;
+    var filteredTodos = todos;
+    
+    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {completed: true});
+    } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, { completed: false });
+    }
+
+    res.json(filteredTodos); 
 });
 
 //GET /todos/:id            //getting each todo module //:id with variable id
@@ -34,7 +43,7 @@ app.get('/todos/:id', function (req, res) {
     }
 });
 
-//POST  /todos request /app.post method will set up an api route
+//POST  /todos request /app.post method will set up an api route 
 
 app.post('/todos', function (req, res) {
      var body = _.pick(req.body, 'description', 'completed');
@@ -52,8 +61,55 @@ app.post('/todos', function (req, res) {
 
 });
 
+//DELETE /todos/:id         //splicing a todo by :id if match is found...
+app.delete('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if(!matchedTodo) {
+        res.status(404).json({"error" : "no todo found with that id"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+        console.log(matchedTodo);
+    }
+});
+
+// UPDATE /todos/:id       //updating an existing content from the todos
+
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if(!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    } else {
+        return res.send(444).json({"error": "I no sabi wetin dhe happen! "});
+    }
+    
+    if (body.hasOwnProperty('description') && _.isString(body.description) 
+        && body.description.trim().length > 0) {
+            validAttributes.description = body.description;
+        } else if(body.hasOwnProperty('description')) {
+            return res.status(400).send();
+        }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
+});
+
 
 app.listen(PORT, function () {
     console.log('Express is listening on PORT: ' + PORT + '!');
 });
 
+        
