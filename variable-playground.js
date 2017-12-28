24519d586e885012e2bb31e4afd4b0410857ff7e
Blarcmann <dtl121ois@gmail.com>
2017-12-28 11:51:21 +0100

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+//     name: 'harhpilo',
+//     age: 22
+// }
+
+// function updatePerson (obj) {
+//     // obj = {
+//     //     name : 'Blarcmann',
+//     //     age: 22
+//     // };
+//     obj.name = "blarcmann";
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//ARRAY example
+
+var arrr = [23, 24, 25];
+
+function updateArrr (arrry) {
+   // arrry = [23, 24, 25, 52];
+   arrry.push(52, 57, 75);
+   debugger;
+}
+
+updateArrr(arrr);
+console.log(arrr);
