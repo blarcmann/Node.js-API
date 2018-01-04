e169ff505f14563c3ea4ace8a918791015096068
Blarcmann <dtl121ois@gmail.com>
2018-01-05 00:49:32 +0100

    Add todo model and post todo

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -4,13 +4,15 @@ var sequelize =  new Sequelize(undefined, undefined, undefined, {
 	'storage': __dirname + '/basic-sqlite-database.sqlite'
 });
 
+//setting the rules for the todo(s) to be accepted
 var Todo = sequelize.define('todo', {
 	description: {
 		type: Sequelize.STRING,
 		allowNull: false,
 		validate: {
 			len: [1, 255]
-		}
+		},
+		defaultValue: 'No description Found'
 	},
 	completed: {
 		type: Sequelize.BOOLEAN,
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _  = require('underscore');
+var db = require('./db.js');
 
 //application parameters
 var app = express();
@@ -54,16 +55,21 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
      var body = _.pick(req.body, 'description', 'completed');
 
+     db.todo.create(body).then(function (todo) {
+        res.json(todo.toJSON());
+     }, function (e) {
+        res.status(400).json(e);
+     });
 
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) 
-                                    || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+    // if(!_.isBoolean(body.completed) || !_.isString(body.description) 
+    //                                 || body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
 
-    body.description = body.description.trim();
-    body.id = todoNextId++;
-    todos.push(body);
-    res.json(body);
+    // body.description = body.description.trim();
+    // body.id = todoNextId++;
+    // todos.push(body);
+    // res.json(body);
 
 });
 
@@ -113,9 +119,11 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-
-app.listen(PORT, function () {
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
     console.log('Express is listening on PORT: ' + PORT + '!');
 });
+});
+
 
         
